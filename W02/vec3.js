5aa8632fa2edd15bd15d25d4e3cf8ae09de09da0
cs16_ninomae <cs16_ninomae@cs16-no-iMac-2.local>
2017-04-17 15:54:53 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -20,3 +20,46 @@ Vec3.prototype.sum = function(){
     return this.x + this.y + this.z;
 
 }
+
+Vec3.prototype.min = function(){
+
+    var a = this.x;
+
+    if(this.y < a)
+	a = this.y;
+
+    if(this.z < a)
+	a = this.z;
+
+    return a;
+
+}
+
+Vec3.prototype.max = function(){
+
+    var a = this.x;
+
+    if(this.y > a)
+	a = this.y;
+
+    if(this.z > a)
+	a = this.z;
+
+    return a;
+
+}
+
+Vec3.prototype.mid = function(){
+
+    var a = this.x;
+    
+    if(this.y != this.max() && this.y != this.min())
+	a = this.y;
+
+    if(this.z != this.max() && this.y != this.min())
+	a = this.z;
+
+    return a;
+
+}
+
