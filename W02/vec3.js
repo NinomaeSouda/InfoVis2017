19b45eb9dc9d77e6fa89aa057d004c8927458c53
cs16_ninomae <cs16_ninomae@cs16-no-iMac-2.local>
2017-04-17 18:08:42 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,65 +1,76 @@
-Vec3 = function(x, y, z){
-    
-    this.x = x;
-    this.y = y;
-    this.z = z;
-
+Vec3=function(x,y,z)
+{
+	console.log(x);
+    this.x = x.value;
+    this.y = y.value;
+    this.z = z.value;
 }
 
-Vec3.prototype.add = function(v){
+// Add method
 
+Vec3.prototype.add = function(v)
+{
     this.x += v.x;
     this.y += v.y;
     this.z += v.z;
     return this;
-
 }
 
-Vec3.prototype.sum = function(){
-
+// Sum method
+Vec3.prototype.sum = function()
+{
     return this.x + this.y + this.z;
-
 }
 
-Vec3.prototype.min = function(){
 
-    var a = this.x;
-
-    if(this.y < a)
-	a = this.y;
-
-    if(this.z < a)
-	a = this.z;
-
-    return a;
-
+//min
+Vec3.prototype.min = function()
+{
+    var min= this.x;
+    if(min>this.y) min=this.y;
+    if(min>this.z) min=this.z; 
+    return min;
 }
 
-Vec3.prototype.max = function(){
 
-    var a = this.x;
-
-    if(this.y > a)
-	a = this.y;
-
-    if(this.z > a)
-	a = this.z;
-
-    return a;
-
-}
-
-Vec3.prototype.mid = function(){
-
-    var a = this.x;
+//mid
+Vec3.prototype.mid = function()
+{
+    var mid;
+    var max= this.x;
+    if(max < this.y) max=this.y;
+    if(max < this.z) max=this.z;
     
-    if(this.y != this.max() && this.y != this.min())
-	a = this.y;
+    var min= this.x;
+    if(min>this.y) min=this.y;
+    if(min>this.z) min=this.z; 
 
-    if(this.z != this.max() && this.y != this.min())
-	a = this.z;
+    if(max>this.x&&min<this.x) mid=this.x;
+    if(max>this.y&&min<this.y) mid=this.y;
+    if(max>this.z&&min<this.z) mid=this.z;
+    
 
-    return a;
+    
+    //var mid=(this.x + this.y + this.z)/3 
+    return mid;
+}
+
+//max
+Vec3.prototype.max = function()
+{
+     var max= this.x;
+    if(max < this.y) max=this.y;
+    if(max < this.z) max=this.z; 
+    return max;
+}
+
+function AreaOfTriangle(v0,v1,v2){
+    
+    var a=Math.pow(v1.x-v0.x,2)+Math.pow(v1.y-v0.y,2)+Math.pow(v1.z-v0.z,2);
+    var b=Math.pow(v2.x-v0.x,2)+Math.pow(v2.y-v0.y,2)+Math.pow(v2.z-v0.z,2);
+    var c=(v1.x-v0.x)*(v2.x-v0.x)+(v1.y-v0.y)*(v2.y-v0.y)+(v1.z-v0.z)*(v2.z-v0.z);
+    var s=Math.sqrt(a*b-c*c)/2;
+    return s;
 
 }
 
